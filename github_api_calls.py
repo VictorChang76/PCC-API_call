8064484ba6df61018c8053c80257707f98bb9a24
VictorChang76 <43318235+VictorChang76@users.noreply.github.com>
2019-07-27 21:04:03 +0800

    Update github_api_calls.py

--- github_api_calls.py
+++ github_api_calls.py
@@ -15,8 +15,9 @@ print(f"Total repositories: {response_dict['total_count']}")
 repo_dicts = response_dict['items']
 print(f'Repositories returned: {len(repo_dicts)}')
 
+# Examine the first repository.
 repo_dict = repo_dicts[0]
-print(f'Keys: {len(repo_dict)}')
+print(f'\nKeys: {len(repo_dict)}')
 
 for key in sorted(repo_dict.keys()):
 	print(key)
